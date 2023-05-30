bf510360a788d4470298575dc2406cf5b1f0df1b
Anton <flppv8@gmail.com>
2023-05-30 19:20:22 +0700

    stylize layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
 import "./App.css";
@@ -6,17 +6,42 @@ import "./App.css";
 axios.defaults.baseURL =
   "https://cors-anywhere.herokuapp.com/https://api-pub.bitfinex.com/v2/";
 
-// add check for t/f type
 // add loading state
-// add styling
-// axios base url
+// break into components
+// fix linter errors
 
 function App() {
   const [tickers, setTickers] = useState([]);
   const [selectedTicker, setSelectedTicker] = useState([]);
   const [candles, setCandles] = useState([]);
   const [trades, setTrades] = useState([]);
-  const [activeTab, setActiveTab] = useState("candles");
+  const [activeTab, setActiveTab] = useState("stats");
+
+  const mainStats = useMemo(() => {
+    if (selectedTicker.length === 0)
+      return {
+        type: "Unknown",
+        symbol: "",
+        price: 0,
+        change: 0,
+      };
+
+    if (selectedTicker[0][0] === "t") {
+      return {
+        type: "Trading pair",
+        symbol: selectedTicker[0].slice(1),
+        price: selectedTicker[7],
+        change: selectedTicker[6] * 100,
+      };
+    } else if (selectedTicker[0][0] === "f") {
+      return {
+        type: "Funding currency",
+        symbol: selectedTicker[0].slice(1),
+        price: selectedTicker[10],
+        change: selectedTicker[9] * 100,
+      };
+    }
+  }, [selectedTicker]);
 
   useEffect(() => {
     async function fetchTickers() {
@@ -30,18 +55,6 @@ function App() {
 
     fetchTickers();
   }, []);
-
-  function renderActiveTabContent() {
-    switch (activeTab) {
-      case "candles":
-        return <pre>{JSON.stringify(candles, null, 2)}</pre>;
-      case "trades":
-        return <pre>{JSON.stringify(trades, null, 2)}</pre>;
-      case "stats":
-      default:
-        return <pre>{JSON.stringify(selectedTicker, null, 2)}</pre>;
-    }
-  }
 
   async function fetchCandles(ticker: string) {
     try {
@@ -62,43 +75,75 @@ function App() {
     }
   }
 
+  function renderActiveTabContent(activeTab: string) {
+    let dataToRender = [];
+
+    switch (activeTab) {
+      case "candles":
+        dataToRender = candles;
+        break;
+      case "trades":
+        dataToRender = trades;
+        break;
+      case "stats":
+      default:
+        dataToRender = selectedTicker;
+        break;
+    }
+
+    return JSON.stringify(dataToRender, null, 2);
+  }
+
   return (
     <main className="wrapper">
-      <div>
-        <select
-          onChange={(e) =>
-            setSelectedTicker(
-              tickers.find((ticker) => ticker[0] === e.target.value)
-            )
-          }
-        >
-          <option>Select a Ticker</option>
-          {tickers.map((ticker) => (
-            <option key={ticker[0]}>{ticker[0]}</option>
-          ))}
-        </select>
-        {selectedTicker.length === 0 && <p>Please select a ticker</p>}
-        {selectedTicker.length > 0 && (
-          <div>
-            <h1>{selectedTicker[0]}</h1>
-            <span>
-              {selectedTicker[0][0] === "t"
-                ? "Trading pair"
-                : "Funding currency"}
-            </span>
-            <h2>
-              {selectedTicker[7]}{" "}
+      <select
+        className="select"
+        onChange={(e) =>
+          setSelectedTicker(
+            tickers.find((ticker) => ticker[0] === e.target.value)
+          )
+        }
+      >
+        <option>Select a Ticker</option>
+        {tickers.map((ticker) => (
+          <option key={ticker[0]}>{ticker[0]}</option>
+        ))}
+      </select>
+
+      {selectedTicker.length === 0 && <p>Please select a ticker</p>}
+
+      {selectedTicker.length > 0 && (
+        <div className="content">
+          <div className="info">
+            <div className="symbol">
+              <span>{mainStats?.symbol}</span>
+              <span className="type">{mainStats?.type}</span>
+            </div>
+            <div className="price">
+              ${mainStats?.price}{" "}
               <sup
                 style={{
-                  color: selectedTicker[6] * 100 >= 0 ? "green" : "tomato",
+                  color: mainStats?.change >= 0 ? "green" : "tomato",
                 }}
               >
-                {(selectedTicker[6] * 100).toFixed(2)}%
+                {mainStats?.change.toFixed(2)}%
               </sup>
-            </h2>
-            <div>
-              <button onClick={() => setActiveTab("stats")}>Stats</button>
+            </div>
+          </div>
+          <div className="data">
+            <div className="controls">
               <button
+                className={
+                  "tab-button" + (activeTab === "stats" ? " active" : "")
+                }
+                onClick={() => setActiveTab("stats")}
+              >
+                Stats
+              </button>
+              <button
+                className={
+                  "tab-button" + (activeTab === "candles" ? " active" : "")
+                }
                 onClick={() => {
                   fetchCandles(selectedTicker[0]);
                   setActiveTab("candles");
@@ -107,6 +152,9 @@ function App() {
                 Candles
               </button>
               <button
+                className={
+                  "tab-button" + (activeTab === "trades" ? " active" : "")
+                }
                 onClick={() => {
                   fetchTrades(selectedTicker[0]);
 
@@ -116,10 +164,11 @@ function App() {
                 Trades
               </button>
             </div>
-            <div className="content">{renderActiveTabContent()}</div>
+
+            <pre>{renderActiveTabContent(activeTab)}</pre>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </main>
   );
 }
